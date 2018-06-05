080b9498451f1756df6cae9677ff6f1da03a0b48
wayne <im.wayne.wy@gmail.com>
2018-06-05 18:06:47 +0800

    add ads use fetch api like axios

---
+++ src/components/HomeAd/index.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class HomeAd extends Component {
+    render(){
+        <div id="home-ad">
+            <h2>Huge Sale! </h2>
+            <div calssName="ad-container clear-fix">
+                {
+                    this.props.data.map((item, index) => {
+                        return(
+                            <div key={index} className="ad-item float-left">
+                                <a href={item.link} target="_blank">
+                                    <img src={item.img} alt={item.title}/>
+                                </a>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        </div>
+    }
+}
+
+export default HomeAd;
